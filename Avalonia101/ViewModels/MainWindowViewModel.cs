faf6a4b2343ca7f830d40c42d5bedfb3b96b17ee
Kevin <kevi3758@gmail.com>
2023-06-19 09:14:19 +0200

    Initial commit

---
+++ Avalonia101/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Avalonia101.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Avalonia101/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Avalonia101.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
