e8a5354ac11dfe49d167ef23097a48746562b86c
Kevin <kevi3758@gmail.com>
2023-06-19 14:29:16 +0200

    Sorting

--- Avalonia101/ViewModels/MainWindowViewModel.cs
+++ Avalonia101/ViewModels/MainWindowViewModel.cs
@@ -3,23 +3,28 @@
 public class MainWindowViewModel : ViewModelBase
 {
     // Add our SimpleViewModel.
-// Note: We need at least a get-accessor for our Properties.
+    // Note: We need at least a get-accessor for our Properties.
+
     public BasicMvvmVm BasicMvvmVm { get; } = new();
     public CommandSample Commands { get; set; } = new();
+
+    #region BasicMvvmSample
+
     public ValidationUsingDataAnnotationViewModel ValidationUsingDataAnnotationViewModel { get; set; } = new();
-    
+
     /// <summary>
     /// Gets a ViewModel showing how to use INotifyDataErrorInfo for validation
     /// </summary>
     public ValidationUsingINotifyDataErrorInfoViewModel ValidationUsingINotifyDataErrorInfoViewModel { get; }
         = new();
-    
+
     /// <summary>
     /// Gets a ViewModel showing how to use Exceptions inside the setter for validation
     /// </summary>
     public ValidationUsingExceptionInsideSetterViewModel ValidationUsingExceptionInsideSetterViewModel { get; }
         = new();
 
-    public ValueConverterVM ValueConverterVM { get; }= new();
+    #endregion
 
+    public ValueConverterVM ValueConverterVM { get; } = new();
 }
