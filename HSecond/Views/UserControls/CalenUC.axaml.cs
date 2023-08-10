ee91544dbb1cef4ce82769f2d8f3dfc13fd9afd4
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-10 09:04:46 +0200

    UserControlsDode

---
+++ HSecond/Views/UserControls/CalenUC.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HSecond.Views.UserControls;
+
+public partial class CalenUC : UserControl
+{
+    public CalenUC()
+    {
+        InitializeComponent();
+    }
+}
