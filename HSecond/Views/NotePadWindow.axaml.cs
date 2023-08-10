76d526b16a415b5b4e4a64a57d061de0cf180442
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-10 13:32:36 +0200

    Calculator Start

---
+++ HSecond/Views/NotePadWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HSecond.Views;
+
+public partial class NotePadWindow : Window
+{
+    public NotePadWindow()
+    {
+        InitializeComponent();
+    }
+}
