3980d776a1dd9636cdeb8262cb9f24470c2fc99d
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-10 13:28:05 +0200

    HomeMade Button Done

--- HSecond/Views/MainWindow.axaml.cs
+++ HSecond/Views/MainWindow.axaml.cs
@@ -1,72 +1,112 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Layout;
+using HSecond.Views.UserControls;
 using Microsoft.VisualBasic;
 
-namespace HSecond.Views;
-#pragma warning disable
-public partial class MainWindow : Window
+namespace HSecond.Views
 {
-    public MainWindow()
+    #pragma warning disable
+    // Partial class for the MainWindow, which is derived from the Window class.
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
-
-    private void CbSectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        if (sender is ComboBox cb)
+        // Constructor for the MainWindow.
+        public MainWindow()
         {
-            ComboBoxItem item = (ComboBoxItem)cb.SelectedItem;
+            InitializeComponent();
+        }
 
-            switch (item.Content)
+        // Event handler for ComboBox selection change related to currency.
+        private void CbSectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox cb)
             {
-                case "EUR":
-                    Currency.Text = (100 * 7.43).ToString();
-                    break;
-                case "USD":
-                    Currency.Text = (100 * 6.76).ToString();
-                    break;
-                case "DKK":
-                    Currency.Text = "100";
-                    break;
+                ComboBoxItem item = (ComboBoxItem)cb.SelectedItem;
+
+                // Calculate and display currency based on selected item.
+                switch (item.Content)
+                {
+                    case "EUR":
+                        Currency.Text = (100 * 7.43).ToString();
+                        break;
+                    case "USD":
+                        Currency.Text = (100 * 6.76).ToString();
+                        break;
+                    case "DKK":
+                        Currency.Text = "100";
+                        break;
+                }
             }
         }
-    }
 
-    private void ChangeWindowName(object? sender, SelectionChangedEventArgs e)
-    {
-        if (sender is ComboBox cb)
+        // Event handler for ComboBox selection change related to window title.
+        private void ChangeWindowName(object? sender, SelectionChangedEventArgs e)
         {
-            string selectedWord = ((ComboBoxItem)cb.SelectedItem).Content.ToString();
-
-            ThisWindow.Title = selectedWord;
-        }
-    }
-
-    private void SDateChange(object? sender, SelectionChangedEventArgs e)
-    {
-        if (sender is Calendar cl)
-        {
-            DateTime date = (DateTime)cl.SelectedDate;
-            if (date.Day is 5)
+            if (sender is ComboBox cb)
             {
-                cl.IsVisible = false;
+                // Update window title with the selected word.
+                string selectedWord = ((ComboBoxItem)cb.SelectedItem).Content.ToString();
+                this.Title = selectedWord;
             }
         }
-    }
 
-    private void SizeUp(object? sender, TextChangedEventArgs e)
-    {
-        if (sender is TextBox tb)
+        // Event handler for Calendar selection change.
+        private void SDateChange(object? sender, SelectionChangedEventArgs e)
         {
-            try
+            if (sender is Calendar cl)
             {
-                tb.FontSize = int.Parse(tb.Text) * 10;
-                Ccalender.IsVisible = true;
+                // Check if the selected date's day is 5 and hide the calendar if true.
+                DateTime date = (DateTime)cl.SelectedDate;
+                if (date.Day is 5)
+                {
+                    cl.IsVisible = false;
+                }
             }
-            catch (Exception exception)
+        }
+
+        // Event handler for TextBox text change related to font size.
+        private void SizeUp(object? sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
             {
-                Console.WriteLine(exception.Message);
+                try
+                {
+                    // Update font size based on entered text and show a specific element.
+                    tb.FontSize = int.Parse(tb.Text) * 10;
+                    Ccalender.IsVisible = true;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
             }
+        }
+
+        // Event handler for a button click.
+        private void ButtonKlikkede(object? sender, object e)
+        {
+            // Looping logic to change text and orientation in the user control.
+            for (int i = 3; i > 0; i++)
+            {
+                HomeName.txtman.Text = "Klikkede";
+
+                if (HomeName.Panel.Orientation == Orientation.Horizontal)
+                {
+                    HomeName.Panel.Orientation = Orientation.Vertical;
+                }
+                else
+                {
+                    HomeName.Panel.Orientation = Orientation.Horizontal;
+                }
+            }
+        }
+
+        // Event handler for the MouseClick event of the user control.
+        private void HomeName_OnMouseClick(object sender, MouseClickEventArgs e)
+        {
+            // Update the window title with a specific message.
+            this.Title = @"Simon is the Winner """"Maybe""";
         }
     }
 }
---
+++ HSecond/Views/UserControls/HomeMadeButtonUC.axaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media.Imaging;
+
+
+namespace HSecond.Views.UserControls;
+
+// Defining a custom UserControl class named HomeMadeButtonUC.
+public partial class HomeMadeButtonUC : UserControl
+{
+    // Defining an event delegate for MouseClick events.
+    public event MouseClickEventHandler MouseClick;
+
+    // Constructor for the HomeMadeButtonUC class.
+    public HomeMadeButtonUC()
+    {
+        // Initialize the user control components.
+        InitializeComponent();
+
+        // Attach the OnPointerReleased method to the PointerReleased event.
+        PointerReleased += OnPointerReleased;
+    }
+
+    // Event handler for the PointerReleased event.
+    void OnPointerReleased(object sender, PointerReleasedEventArgs args)
+    {
+        // Create an instance of the MouseClickEventArgs class.
+        MouseClickEventArgs mouseClickArgs = new();
+
+        // Determine the mouse button that was pressed.
+        switch (args.InitialPressMouseButton)
+        {
+            case MouseButton.Left:
+                mouseClickArgs.MouseButton = 1; // Left mouse button.
+                break;
+            case MouseButton.Right:
+                mouseClickArgs.MouseButton = 2; // Right mouse button.
+                break;
+            case MouseButton.Middle:
+                mouseClickArgs.MouseButton = 3; // Middle mouse button.
+                break;
+        }
+
+        // Check if there are any subscribers to the MouseClick event.
+        if (MouseClick != null)
+        {
+            // Invoke the MouseClick event and pass the sender and event arguments.
+            MouseClick.Invoke(this, mouseClickArgs);
+        }
+    }
+}
+
+// Delegate declaration for the MouseClick event.
+public delegate void MouseClickEventHandler(object sender, MouseClickEventArgs e);
+
+// Custom event arguments class for MouseClick events.
+public class MouseClickEventArgs : EventArgs
+{
+    // Property to store the mouse button that was clicked.
+    public int MouseButton { get; set; }
+}
