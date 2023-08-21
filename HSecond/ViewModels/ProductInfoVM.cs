42d8a609198cf67cabbba8d2049f67c471ef11a6
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-21 12:09:08 +0200

    PRoduct VM satart

---
+++ HSecond/ViewModels/ProductInfoVM.cs
@@ -0,0 +1,22 @@
+﻿using HSecond.Models;
+
+namespace HSecond.ViewModels;
+
+public class ProductInfoVM : ViewModelBase
+{
+    public ProductInfoVM(ItemModel selectedItem)
+    {
+        _selectedItem = selectedItem;
+    }
+
+    public ItemModel SelectedItem
+    {
+        get => _selectedItem;
+        set
+        {
+            _selectedItem = value;
+            OnPropertyChanged();
+        }
+    }
+    private ItemModel _selectedItem { get; set; }
+}
