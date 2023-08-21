00404d2203929c221673fcb201d8485d02170611
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-21 12:40:46 +0200

    ..

---
+++ HSecond/ViewModels/ProdukteringVM.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive;
+using Avalonia.Controls;
+using HSecond.Models;
+using ReactiveUI;
+
+namespace HSecond.ViewModels;
+
+public class ProdukteringVM : ViewModelBase
+{
+    public ProdukteringVM()
+    {
+        ItemClickedCommand = ReactiveCommand.Create<object>(ShowItemInfo);
+    }
+
+    private void ShowItemInfo(object obj)
+    {
+        if (obj is not ItemModel)
+            return;
+
+        CurrentView = new ProductInfoVM((ItemModel)obj);
+    }
+
+    public ReactiveCommand<object, Unit> ItemClickedCommand { get; }
+
+    public object CurrentView
+    {
+        get { return _currentView; }
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private object _currentView;
+}
