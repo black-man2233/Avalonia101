f4655c93e5ba78a388bf81f706300b8e53f263db
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-21 12:32:29 +0200

    ProductInFOVIew Created

---
+++ HSecond/Views/UserControls/Produktering/ProductInfoView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HSecond.Views.UserControls.Produktering;
+
+public partial class ProductInfoView : UserControl
+{
+    public ProductInfoView()
+    {
+        InitializeComponent();
+    }
+}
