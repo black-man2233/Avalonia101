52f96267e0067aa73496929a9791a730de0df904
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-08-21 11:58:29 +0200

    MenuVM almost prepared

---
+++ HSecond/ViewModels/MenuVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
+using HSecond.Models;
+using ReactiveUI;
+
+namespace HSecond.ViewModels;
+
+public class MenuVM
+{
+    public MenuVM()
+    {
+        ItemClickedCommand = ReactiveCommand.Create<object>(ShowItemInfo);
+    }
+
+
+    public ReactiveCommand<object, Unit> ItemClickedCommand { get; }
+
+    #region Methods
+
+    private void ShowItemInfo(object parameter)
+    {
+        try
+        {
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    #endregion
+
+    #region private fields
+
+    private ObservableCollection<ItemModel> _menuList { get; set; }
+
+    #endregion
+}
